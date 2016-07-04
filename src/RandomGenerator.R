819042ccfb42acdeff1880be8284d49a21f62318
nyasui <nikolaus.yasui@mail.mcgill.ca>
2016-07-04 17:58:35 +0900

    major rewrite of RandomGenerator.R

--- src/RandomGenerator.R
+++ src/RandomGenerator.R
@@ -1,100 +1,113 @@
+#!/usr/bin/Rscript
+'Usage: 
+    RandomGenerator.R <individuals>... -e=<genes> [-p=<prob> -o=<outfile>]
+    RandomGenerator.R <infile> -s=<species> [-p=<prob> -o=<outfile>]
+
+options:
+ -h --help                          Show this screen.
+
+ -e <genes>, --genes <genes>        Number of genes.
+                                    [default: 1]
+
+ <infile>                           Filepath if using existing trees.
+                                    [default: NULL]
+
+ <individuals>                      Number of individuals per species.
+                                    [default: 4]
+
+ -o <outfile>, --outfile <outfile>  Root of filepath of outfile.
+                                    (script will output outfile.txt and
+                                    outfile_true.txt).
+                                    [default: test]
+
+ -p <prob>, --prob <prob>           Probability of a leaf going missing.
+                                    [default: 0.2]
+
+ -s <species>, --species <species>  Number of species.
+                                    [default: 5]
+
+Either reads a .nex file, or creates num_genes trees and randomly 
+removes their leaves with probability p.' -> doc
+
+library('docopt')
 library('ape')
-library('phytools')
-source('remove_random_tips.R')
+source('tools.R')
 # Load Libraries
 
-#   ########################################################################
-#   # some code for loading nexus files
-#   filepath <- 'phyclust/simulated_data/SD:Ne ratio sweep/r_nexus'
-#   files <- list.files(path=filepath, full.names=TRUE)
+# Some useful functions
+drop_n_tips <- function(tree, n){
+    indices <- sample(as.numeric(length(tree$tip.label)), n)
+    drop.tip(tree, tip=indices)
+}
 
-#   # remove Species Trees
-#   tfiles <- files[!unlist(lapply(files, function(f) grepl("Species_Trees", f)))]
+drop_random_tips <- function(tree, rand_fun){
+    tips <- sample(as.numeric(length(tree$tip.label)), rand_fun())
+    drop.tip(tree, tip=tips)
+}
 
-#   trees_list <- lapply(tfiles, read.nexus)
-#   list_of_multiPhylos_of_smaller_trees <- lapply(trees_list, 
-#                                                  drop_tips,
-#                                                  num_to_drop=4)
-#    ########################################################################
+drop_one <- function(tree){
+    dist_matrix <- cophenetic(tree)
 
+    # select a leaf to ignore (JUST ONE FOR NOW)
+    sel <- sample(as.numeric(length(tree$tip.label)), 1)
+
+    # write the actual distance
+    distances <- as.vector(as.dist(dist_matrix))
+
+    # ignore the leaf
+    dist_matrix[sel,] <- -1
+    dist_matrix[,sel] <- -1
+
+    # return both vectorized distance matrices
+    list(distances, as.vector(as.dist(dist_matrix)))
+}
+
+write_matrices <- function(distances){
+    actualDist <- distances[1]
+    alteredDist <- distances[2]
+
+    # write actual distance matrix
+    write(paste(c(format(actualDist, digits=2)), collapse=" "), 
+          fnameTRUE,
+          append=TRUE)
+    # write altered distance matrix
+    write(paste(c(format(alteredDist, digits=2)), collapse=" "),
+          fname,
+          append=TRUE)
+}
+
+# Quality of Life setup
+options(digits=2, error=traceback)
+opts <- docopt(doc)
 
 # Set output files -- fname=the output in the format needed, fnameTRUE=the true distances
-fname <- "test.txt"
-fnameTRUE <- "trueTest.txt"
+fname <- paste(opts$outfile, '.txt', sep='')
+fnameTRUE <- paste(opts$outfile, '_true.txt', sep='')
+prob_missing <- assert_valid_prob(as.numeric(opts$prob))
 
-# Parameters: numSpecies, numLeafs, probability of missing leaf
-numSpecies <- 5
-numLeafs <- 4
-prob_missing <- 0.2
+if (!is.null(opts$infile)) {
+    # get trees
+    trees <- read.nexus(opts$infile)
 
-# Output set to 2 decimal digits
-options(digits=2)
+    n_species <- as.numeric(opts$species)
+    n_genes <- length(trees)
+    n_leaves <- length(trees[[1]]$tip.label)
+    n_individuals <- replicate(n_species, n_leaves / n_species)
 
-# Write the number of species, number of leafs in the file
-write(c(numSpecies,numLeafs), fname, append=TRUE)
+} else {
+    n_species <- length(opts$individuals)
+    n_individuals <- as.numeric(opts$individuals)
+    n_genes <- as.numeric(opts$genes)
+    n_leaves <- sum(n_individuals)
 
-# Now I have the population of every species to be up to 10: THIS CAN BE CHANGED
-poss_Nums <- 1:10
-
-# Creating Random numbers of individuals for each species
-numIndividuals <- c();
-for (j in 1:numSpecies){
-    numIndividuals <- c(numIndividuals, sample.int(10, size = 1, replace = TRUE));
+    trees <- lapply(vector(,n_genes), function(x) rcoal(n_leaves))
 }
 
-# Writing the numbers of individuals in file
-for (j in 1:numSpecies){
-    write(numIndividuals[j], fname, append=TRUE)
-}
-write("\n", fname, append=TRUE)
+# Write the number of species, number of leaves to the file
+invisible(write(c(n_species, n_leaves), fname, append=TRUE))
 
+# write the number of individuals to file
+invisible(lapply(n_individuals, write, file=fname, append=TRUE))
+invisible(write("\n", fname, append=TRUE))
 
-# Creating a Sample to choose from for leafs missing
-sam <- 1:numLeafs
-
-# For every species
-for (j in 1:numSpecies){
-    # For every individual in that species
-    for (i in 1:numIndividuals[j]){
-        # Create random tree with numLeafs leafs
-        tree <- rcoal(numLeafs)
-        # Calculate the distances in vectorized form
-        dist <- cophenetic(tree)
-        # Randomly select some trees to be missing a leaf
-        random <- runif(1, 0, 1)
-        if (random < prob_missing){
-            # Randomly select which leaf is missing: NOW ONLY ONE LEAF MISSING
-            sel <- sample(sam, 1)
-        }
-        else{
-            sel <- -1
-        }
-        # Create vectors for actual and altered Distances
-        actualDist <- c()
-        alteredDist <- c()
-        for (k in 1:(numLeafs-1)){
-            for (l in (k+1):numLeafs){
-                x1 <- paste("t",k,sep="")
-                y1 <- paste("t",l,sep="")
-                # Write the actual distances in the format needed
-                actualDist <- c(actualDist, dist[x1,y1])
-                # If the leaf is missing, set its altered distance to -1
-                if (sel==k || sel==l){
-                    alteredDist <- c(alteredDist, -1)
-                }
-                else{
-                    alteredDist <- c(alteredDist, dist[x1,y1])
-                }
-            }
-        }
-        # Write altered and actual distances in the format of the file
-    write(paste(c(format(actualDist, digits=2)), collapse=" "), fnameTRUE ,append=TRUE)
-    
-    write(paste(c(format(alteredDist, digits=2)), collapse=" "), fname ,append=TRUE)
-    }
-    # Leave an empty line between species (for easy visual separation)
-    write("\n", fnameTRUE, append=TRUE)
-    write("\n", fname, append=TRUE)
-   
-}
-
+invisible(lapply(trees, function(x) write_matrices(drop_one(x))))
