dccf7c86b983e6b4e28a1a1b602585b48bdcabc3
nyasui <nikolaus.yasui@mail.mcgill.ca>
2016-08-02 17:55:22 +0900

    Updated experimental setup to capture more useful data and reduce ambiguity

--- src/experiment.py
+++ src/experiment.py
@@ -55,19 +55,19 @@ if __name__ == "__main__":
     if test:
         c = [0.6]#, 0.7, 0.8, 0.9, 1, 2, 4, 6, 8, 10, 20]
         c = list(map(float, c))
-        genes = [10, 20]#, 40, 50, 60]
-        inds = [10]#, 6, 10]
-        methods = [3]#, 4]
+        genes = [10]#, 40, 50, 60]
+        inds = [5]#, 6, 10]
+        methods = [1,2]#, 4]
         probs = [0.1]#, 0.05, 0.2]
         species = [2]#, 3, 5]
         pop_size = [10000]
         depth = list(set(map(lambda x: int(x[0]*x[1]), product(c, pop_size))))
         trees = [1] # number of species trees
-        flow_dict = {"all":True,
+        flow_dict = {"all": True,
                      "generate":False,
                      "drop":False,
                      "impute":False,
-                     "analyze":False, 
+                     "analyze":True, 
                      "--plus":True}
 
     # experimental set up
@@ -75,13 +75,13 @@ if __name__ == "__main__":
         c = [0.6, 0.7, 0.8, 0.9, 1, 2, 4, 6, 8, 10, 20]
         c = list(map(float, c))
         genes = [10, 20, 30]
-        inds = [2, 5, 10]
-        methods = [3, 4]
-        probs = [0.1, 0.05, 0.2]
-        species = [2, 3, 5]
+        inds = [5, 10, 15]
+        methods = [1, 2]
+        probs = [4]
+        species = [3]
         pop_size = [10000]
         depth = list(set(map(lambda x: int(x[0]*x[1]), product(c, pop_size))))
-        trees = [1] # number of species trees
+        trees = [3] # number of species trees
         
         # Options to set 
         flow_dict = {"all":True,        # overrides other options
@@ -93,14 +93,14 @@ if __name__ == "__main__":
                                         # the first selection operation
     
     # batch folder naming scheme
-    batch_folder = os.path.join(exp_folder, batch_general)
-    one_batch_run = setup(batch_folder, methods, probs, flow_dict, overwrite)
+    batch_folder = os.path.join(exp_folder, tools.batch_general)
+    one_batch_run = setup(batch_folder, methods, probs, flow_dict, force)
     batch_iterator = product(depth, genes, inds, pop_size, species, trees)
     
     # choose run method depending on testing or not
     run_parallel= lambda: tools.parmap(one_batch_run, batch_iterator)
     run_serial = lambda: list(map(one_batch_run, batch_iterator))
-    f = run_parallel if experiment else run serial
+    f = run_parallel if experiment else run_serial
 
     # run experiment
     tools.timeit(f, "solving all problems")
